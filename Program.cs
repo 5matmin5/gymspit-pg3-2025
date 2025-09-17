ebcbde6d958b3096f321831969ce5a5a9c12ccc8
5matmin5 <5matmin5@gmail.com>
2025-09-17 09:00:24 +0200

    Madlips lecture 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//napiš program který se tě zeptá na barvu vlastnosti, zvíře, jídlo a místo. Poté ti vygeneruje krátký příběh s použitím těchto slov.
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Vítej v Mad Libs! Odpověz na následující otázky:");
+        
+        Console.Write("Zadej barvu: ");
+        string barva = Console.ReadLine();
+        
+        Console.Write("Zadej zvíře: ");
+        string zvire = Console.ReadLine();
+       
+        Console.Write("Zadej jídlo: ");
+        string jidlo = Console.ReadLine();
+       
+        Console.Write("Zadej místo: ");
+        string misto = Console.ReadLine();
+        string pribeh = $"Jednoho dne jsem viděl/a {barva} {zvire}, které jedlo {jidlo} v {misto}. Bylo to neuvěřitelné!";
+       
+        Console.WriteLine("\nTady je tvůj příběh:");
+        Console.WriteLine(pribeh);
+    }
+}
